0b64e56b24dcfac679adbd2091ffb5ff47c1ff43
HikingCarrot7 <47760015+HikingCarrot7@users.noreply.github.com>
2019-03-10 18:53:31 -0600

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,20 @@
+/*Imprime su nombre completo y la dirección de su domicilio.
+Fecha: 17/ene/2019
+Version: 1.0 */
+
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+	
+	//Mostrar línea por pantalla
+	printf ("\nMi nombre es: Ricardo Nicolás Canul Ibarra");
+	
+	//Mostrar segunda línea por pantalla
+	printf ("\n\nDomicilio: Calle 19 B POR 8 y 10 #30\nCol Lázaro Cárdenas");
+	
+	//Esperar un caracter del teclado
+	getch();
+	
+	return 0;
+}
